d12f2107f49c2788e7069f5984def2b167b2f5a9
Benni <benjamin@sendiradid.is>
2021-07-27 11:02:46 +0000

    Triggerlist

--- src/components/triggers/index.tsx
+++ src/components/triggers/index.tsx
@@ -1,17 +1,28 @@
-import { Select } from '@rebass/forms';
-import ObsWebSocket from 'obs-websocket-js';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Heading } from 'rebass';
 import { Trigger, useClients } from '../clients/clientContext';
 import TriggerForm from './triggerForm/triggerForm';
+import TriggerList from './triggerList/triggerList';
 
 const Triggers = () => {
 	const { clients, setClients, updated } = useClients();
-	// const [showForm, setShowForm] = useState<boolean>(false);
 
 	const saveTrigger = (trigger: Trigger) => {
 		const clientsNew = clients;
 		clientsNew[trigger.when.clientName].triggers.push(trigger);
+		setClients(clientsNew);
+	};
+
+	const mappedThens = (entry: Trigger) => {
+		return entry.then.map((x) => x.clientName + '//' + x.sceneName).join(';');
+	};
+
+	const deleteTrigger = (trigger: Trigger) => {
+		const clientsNew = clients;
+		const index = clientsNew[trigger.when.clientName].triggers.findIndex(
+			(x) => mappedThens(x) === mappedThens(trigger)
+		);
+		clientsNew[trigger.when.clientName].triggers.splice(index, 1);
 		setClients(clientsNew);
 	};
 
@@ -21,9 +32,8 @@ const Triggers = () => {
 	return (
 		<div className='Triggers'>
 			<Heading type='h3'>Triggers</Heading>
-			{/* Button for adding a trigger */}
-			{/* Active triggers */}
 			<TriggerForm saveTrigger={saveTrigger} />
+			<TriggerList deleteTrigger={deleteTrigger} />
 		</div>
 	);
 };
---
+++ src/components/triggers/triggerList/triggerList.tsx
@@ -0,0 +1,44 @@
+import { FC, useEffect } from 'react';
+import { Button, Flex, Text } from 'rebass';
+import { Trigger, useClients } from '../../clients/clientContext';
+
+interface Props {
+	deleteTrigger: (trigger: Trigger) => void;
+}
+
+const TriggerList: FC<Props> = ({ deleteTrigger }) => {
+	const { clients } = useClients();
+
+	return (
+		<Flex>
+			{Object.keys(clients).map((clientName) => {
+				if (clients[clientName].triggers.length > 0) {
+					return clients[clientName].triggers.map((trigger) => {
+						return (
+							<Flex>
+								<Text>
+									When: {trigger.when.clientName} - {trigger.when.sceneName}
+								</Text>
+								<Flex ml={4} justifyContent='space-between'>
+									<Text>Then:</Text>
+									{trigger.then.map((entry) => {
+										return (
+											<Text>
+												{entry.clientName} - {entry.sceneName}
+											</Text>
+										);
+									})}
+								</Flex>
+								<Button backgroundColor='red' onClick={() => deleteTrigger(trigger)}>
+									X
+								</Button>
+							</Flex>
+						);
+					});
+				}
+			})}
+		</Flex>
+	);
+};
+
+export default TriggerList;
